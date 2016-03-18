434dd75707830b6cd6498358ed761689ca897fed
Matthew Jordan <matthew@technidragon.Home>
2016-03-17 20:22:13 -0400

    Added variable defining the Elinks config directory.

--- settings/cli_apps.zsh
+++ settings/cli_apps.zsh
@@ -4,3 +4,6 @@ export WINEPREFIX=$XDG_DATA_HOME/winehq
 
 # Less history file location
 export LESSHISTFILE=$XDG_DATA_HOME/lesshistory
+
+# Elinks config directory
+export ELINKS_CONFDIR=$XDG_CONFIG_HOME/elinks
