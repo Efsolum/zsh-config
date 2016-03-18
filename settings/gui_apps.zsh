c387bff58648a64bc884982d363c5b22763e1f99
Matthew Jordan <matthew@technidragon.Home>
2016-03-17 20:25:09 -0400

    Updated Calibre related environment variables.

---
+++ settings/gui_apps.zsh
@@ -0,0 +1,8 @@
+# Calibre settings
+export CALIBRE_CONFIG_DIRECTORY=$XDG_CONFIG_HOME/calibre
+export CALIBRE_TEMP_DIR=$XDG_CACHE_HOME/calibre
+export CALIBRE_CACHE_DIRECTORY=$XDG_DATA_HOME/calibre
+
+# [[ -d $CALIBRE_TEMP_DIR ]] || \
+# 		mkdir -p $CALIBRE_TEMP_DIR
+
