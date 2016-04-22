b2d97a57e2971cc724e031bf0e2c8f30cdaa8a78
Matthew Jordan <matthew@technidragon.Home>
2016-04-22 08:42:48 -0400

    Added the ZSH_THEME variable(s) for future reference.

--- Zplug.zsh
+++ Zplug.zsh
@@ -10,17 +10,22 @@ source $ZPLUG_HOME/zplug
 zplug "b4b4r07/zplug" # ZSH & CLI Package Manager
 
 zplug "caiogondim/bullet-train-oh-my-zsh-theme", of:bullet-train.zsh-theme
+# export ZSH_THEME='bullet-train'
 export BULLETTRAIN_CONTEXT_SHOW=true
 export BULLETTRAIN_EXEC_TIME_SHOW=true
 
 # zplug "zakaziko99/agnosterzak-ohmyzsh-theme", of:agnosterzak.zsh-theme
+# export ZSH_THEME='agnosterzak'
 
 # zplug "Ciacho/ciacho-ohmyzsh-theme", of:ciacho.zsh-theme
+# export ZSH_THEME='ciacho'
 
 # zplug "bhilburn/powerlevel9k", of:powerlevel9k.zsh-theme
+# export ZSH_THEME='powerlevel9k/powerlevel9k'
 # export POWERLEVEL9K_PROMPT_ON_NEWLINE=true
 
 # zplug "el1t/statusline", of:"oh-my-zsh/statusline.zsh-theme"
+# export ZSH_THEME='oh-my-zsh/statusline'
 
 zplug "plugins/battery", from:oh-my-zsh
 zplug "plugins/git", from:oh-my-zsh
